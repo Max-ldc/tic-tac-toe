fedefcdba36b72e8647a3edb55bc4d17404dbc43
Maxence Leduc <maxence.leduc@yahoo.com>
2023-02-19 20:41:50 +0100

    multiplayer adjustments ; add who's turn is info

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 var jeu = document.querySelector(".jeu");
-let joueur = false;
 let circlesTable = [];
 let crossesTable = [];
-let modal = document.querySelector(".modal");
+var modal = document.querySelector(".modal");
 
 function newGameMulti(){
+    let joueur = false;
+    majPlayer(joueur);
     modal.style = "display : none"; // on enlève la modal en début de partie
     let nbrCaseRemplie = 0;         // on passe le compteur de cases remplies à 0
     circlesTable = [0,0,0,0,0,0,0,0,0]; // aucun cercle donc toutes les pos à 0
     crossesTable = [0,0,0,0,0,0,0,0,0]; // pareil pour les croix
-
+    
     let cases = document.querySelectorAll(".case"); // Au reset : on prend toutes les cases et on les supprime
     cases.forEach((e) => {
         e.remove();
     })
-
+    
     for (i=0; i<9; i++){               // création des 9 cases
         let caseRemplie = false;            // la case n'est pas remplie de base
         let addCase = document.createElement("div");
@@ -35,80 +36,109 @@ function newGameMulti(){
                 caseRemplie = true;             // la case passe en remplie, on ne pourra rien ajouter
                 addCase.appendChild(addSign);   // On ajoute le signe à la case
                 joueur = !joueur;               // On change de joueur après chaque signe placé
+                majPlayer(joueur);
                 nbrCaseRemplie++;               // On compte le nombre de cases remplies pour arrêter le jeu (à faire)
-                checkWin(nbrCaseRemplie);       // On regarde si le jeu est fini (win ou plus de case)
+                if (nbrCaseRemplie < 9){    // Doit-on continuer le jeu en cas de non-victoire ?
+                    checkWin(true);             // si moins de 9 cases remplies, oui
+                }else{
+                    checkWin(false);            // Si 9 cases remplies, non
+                }
             }
         }
     jeu.appendChild(addCase);               // On ajoute la case au jeu
     }
 }
 
-function checkWin(x){
-    if(x < 10){
-        for (i=0;i<7;i=i+3){            // On test les lignes : 0/1/2 | 3/4/5 | 6/7/8
-            // test pour les ronds
-            if( circlesTable[i] && circlesTable[i+1] && circlesTable[i+2] ){
-                colorThreeCases(i,i+1,i+2);
-                // modal.style = "display : block";
-                modal.style = "display : flex";
-                modal.innerHTML = `Bravo, les ronds gagnent !`
+function checkWin(x){           // Test de toutes les combinaisons de win possibles ou si les 9 cases sont remplies.
+    for (i=0;i<7;i=i+3){            // On test les lignes : 0/1/2 | 3/4/5 | 6/7/8
+        // test pour les ronds
+        if( circlesTable[i] && circlesTable[i+1] && circlesTable[i+2] ){
+            colorThreeCases(i,i+1,i+2);
+            changeModal("circle");
+            x = true;
             // test pour les croix
-            }else if( crossesTable[i] && crossesTable[i+1] && crossesTable[i+2] ){
-                colorThreeCases(i,i+1,i+2);
-                modal.style = "display : flex";
-                modal.innerHTML = `Bravo, les croix gagnent !`
-            }
+        }else if( crossesTable[i] && crossesTable[i+1] && crossesTable[i+2] ){
+            colorThreeCases(i,i+1,i+2);
+            changeModal("cross");
+            x = true;
         }
-        for (i=0;i<4;i++){              // on test les colonnes 1/4/7 | 2/5/8 | 3/6/9
-            // test pour les ronds
-            if ( circlesTable[i] && circlesTable[i+3] && circlesTable[i+6] ){
-                colorThreeCases(i,i+3,i+6);
-                modal.style = "display : flex";
-                modal.innerHTML = `Bravo, les ronds gagnent !`;
+    }
+    for (i=0;i<4;i++){              // on test les colonnes 1/4/7 | 2/5/8 | 3/6/9
+        // test pour les ronds
+        if ( circlesTable[i] && circlesTable[i+3] && circlesTable[i+6] ){
+            colorThreeCases(i,i+3,i+6);
+            changeModal("circle");
+            x = true;
             // test pour les croix
-            }else if ( crossesTable[i] && crossesTable[i+3] && crossesTable[i+6] ){
-                colorThreeCases(i,i+3,i+6);
-                modal.style = "display : flex";
-                modal.innerHTML = `Bravo, les croix gagnent !`
-            }
+        }else if ( crossesTable[i] && crossesTable[i+3] && crossesTable[i+6] ){
+            colorThreeCases(i,i+3,i+6);
+            changeModal("cross");
+            x = true;
         }
-        // test conditons diagonales pour les cercles
-        if( circlesTable[0] && circlesTable[4] && circlesTable[8] ){
-            colorThreeCases(0,4,8);
-            modal.style = "display : flex";
-            modal.innerHTML = `Bravo, les ronds gagnent !`;
+    }
+    // test conditons diagonales pour les cercles
+    if( circlesTable[0] && circlesTable[4] && circlesTable[8] ){
+        colorThreeCases(0,4,8);
+        changeModal("circle");
+        x = true;
         // 2e diago pour les cercles
-        }else if( circlesTable[2] && circlesTable[4] && circlesTable[6] ){
-            colorThreeCases(2,4,6);
-            modal.style = "display : flex";
-            modal.innerHTML = `Bravo, les ronds gagnent !`;
+    }else if( circlesTable[2] && circlesTable[4] && circlesTable[6] ){
+        colorThreeCases(2,4,6);
+        changeModal("circle");
+        x = true;
         // test conditions diagonales pour les croix
-        }else if( crossesTable[0] && crossesTable[4] && crossesTable[8] ){
-            colorThreeCases(0,4,8);
-            modal.style = "display : flex";
-            modal.innerHTML = `Bravo, les croix gagnent !`
+    }else if( crossesTable[0] && crossesTable[4] && crossesTable[8] ){
+        colorThreeCases(0,4,8);
+        changeModal("cross");
+        x = true;
         // 2e diago pour les croix
-        }else if( crossesTable[2] && crossesTable[4] && crossesTable[6] ){
-            colorThreeCases(2,4,6);
-            modal.style = "display : flex";
-            modal.innerHTML = `Bravo, les croix gagnent !`
-        }
-    }else{                    // si les 9 cases ont été remplies (il faudra inverser en cas de win au 9ème coup)
-        let cases = document.querySelectorAll(".case");
-        cases.forEach((e) => {          // On colore chaque case en darkred
-            e.style = "background-color : darkred"
-        });
-        modal.style = "display : flex";
-        modal.innerHTML = `C'est une égalité !`
+    }else if( crossesTable[2] && crossesTable[4] && crossesTable[6] ){
+        colorThreeCases(2,4,6);
+        changeModal("cross");
+        x = true;
+    }
+    if (!x){    // x est resté à false tout le long = on doit stopper le jeu car toutes les cases sont remplies + il n'y a eu aucune victoire
+        changeModal("tie");
     }
 }
 
-function colorThreeCases(a,b,c){
+function colorThreeCases(a,b,c){        // Colore les 3 cases gagnantes
     let cases = document.querySelectorAll(".case")
         let t = [a,b,c];    // on récupère les 3 indices de cases en question
         t.forEach((e) => {      // pour les 3 cases de win, on les colore en lightgreen
             cases[e].style = "background-color : lightgreen";
         })
+}
+
+function majPlayer(x){                  // Mise à jour du joueur qui doit jouer
+    let infoJoueur = document.querySelector(".infoPlayer");
+    if (x == "end"){                // si c'est un maj de fin
+        infoJoueur.innerHTML = `Appuyez en bas pour rejouer`
+    }else if (x){                   // si c'est aux ronds de jouer
+        infoJoueur.innerHTML = `Au tour de J2 (ronds)`;
+    }else if (!x){                  // si c'est aux croix de jouer
+        infoJoueur.innerHTML = `Au tour de J1 (croix)`;
+    }
+}
+
+function changeModal(c){
+    if (c == "circle"){
+        modal.style = "display : flex";
+        modal.innerHTML = `Bravo, les ronds gagnent !`
+        majPlayer("end");
+    }else if (c == "cross"){
+        modal.style = "display : flex";
+        modal.innerHTML = `Bravo, les croix gagnent !`
+        majPlayer("end");
+    }else if (c == "tie"){
+        let cases = document.querySelectorAll(".case");
+        cases.forEach((e) => {          // On colore chaque case en darkred
+        e.style = "background-color : darkred";
+        });
+        modal.style = "display : flex";
+        modal.innerHTML = `C'est une égalité !`;
+        majPlayer("end");
+    }
 }
 
 newGameMulti();
